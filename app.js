fc9b1916da24381d9b4b6be5158240189b94c0cd
Sanjeet <sanjeetpandey010@gmail.com>
2021-01-27 19:00:06 +0545

    booking route done

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const trip_db = require('./database/trip_db');
 const customersRoute = require('./routes/customersRoute');
+const bookingRoute = require('./routes/bookingRoute');
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(customersRoute);
+app.use(bookingRoute)
 app.listen(90);
 
---
+++ routes/bookingRoute.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const Booking = require('../models/bookingModel');
+const {check, validationResult} = require('express-validator');
+const authCustomer = require('../middleware/authCustomer');
+const jwt = require('jsonwebtoken')
+router.post('/booking/submit',[
+    check('email',"Email is required!").not().isEmpty(),
+    check('username', "It is not valid username").isAlpha()
+],function(req,res){
+    const errors = validationResult(req);
+    if(errors.isEmpty()){
+    const fullname = req.body.fullname;
+    const email = req.body.email;
+    const username = req.body.username;
+    const destination = req.body.destination;
+    const no_of_people = req.body.no_of_people;
+    const departure = req.body.departure;
+    const arrival = req.body.arrival;
+    const phone = req.body.phone;
+    const data = new Booking({
+        fullname:fullname,
+        email:email,
+        username:username,
+        destination:destination,
+        no_of_people:no_of_people,
+        departure:departure,
+        arrival:arrival,
+        phone:phone
+    })
+    data.save()
+    .then(function(result){
+        res.status(201).json({message : "Booking Success!!!" })
+
+    })
+    .catch(function(error){
+        res.status(500).json({message : error })
+    })
+}
+else
+{
+res.status(400).json(errors.array())
+}
+})
+//delete
+router.delete('/booking/delete/:id', function(req,res){
+    const id = req.params.id;
+    Booking.deleteOne({_id : id})
+    .then(function(result){
+        res.status().json({message : "Booking deleted!!"})
+    })
+    .catch(function(error){
+        res.status(500).json({message : error})
+    })
+})
+//update
+router.put('/booking/update/:id', function(req,res){
+    const id = req.params.id;
+    const fullname = req.body.fullname;
+    const email = req.body.email;
+    const username = req.body.username;
+    const destination = req.body.destination;
+    const no_of_people = req.body.no_of_people;
+    
+    Booking.updateOne({_id : id}, {destination : destination, no_of_people : no_of_people })
+    .then(function(result){
+        res.status(200).json({message : "Booking Updated"})
+    })
+    .catch(function(error){
+        res.send().json({error : error})
+    })
+
+})
+module.exports = router;
