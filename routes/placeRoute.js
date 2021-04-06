b63ca218b2d60249e4ed9ddaa07240db71da4ec9
Sanjeet <sanjeetpandey010@gmail.com>
2021-04-06 13:03:21 +0545

    single and fetch added

--- routes/placeRoute.js
+++ routes/placeRoute.js
@@ -8,74 +8,91 @@ const upload = require('../middleware/imageUpload');
 const jwt = require('jsonwebtoken');
 
 router.post('/place/add', upload.single('pimage'), function (req, res) {
-  console.log(req.file);
-  if (req.file == undefined) {
-    return res.status(500).json({ message: 'Invalid file format' });
-  }
-  const placename = req.body.placename;
-  const pdesc = req.body.pdesc;
-  const pprice = req.body.pprice;
+    console.log(req.file);
+    if (req.file == undefined) {
+        return res.status(500).json({ message: 'Invalid file format' });
+    }
+    const placename = req.body.placename;
+    const pdesc = req.body.pdesc;
+    const pprice = req.body.pprice;
 
 
-  const data = new Place({
-    placename: placename,
-    pdesc: pdesc,
-    placeprice: placeprice,
-    pimage: req.file.filename,
-  });
-
-  data
-    .save()
-    .then(function (result) {
-      // success
-      res.status(200).json({ message: 'Place added successfully' });
-    })
-    .catch(function (error) {
-      res.status(500).json({ message: error });
+    const data = new Place({
+        placename: placename,
+        pdesc: pdesc,
+        placeprice: placeprice,
+        pimage: req.file.filename,
     });
+
+    data
+        .save()
+        .then(function (result) {
+            // success
+            res.status(200).json({ message: 'Place added successfully' });
+        })
+        .catch(function (error) {
+            res.status(500).json({ message: error });
+        });
 });
 
 //for delete
 
 router.delete('/place/delete/:id', function (req, res) {
-  // extra -- check if id exists
-  const pid = req.params.id;
-  Product.deleteOne({ _id: id })
-    .then(function (result) {
-      res.status().json({ message: 'Product deleted' });
-    })
-    .catch(function (error) {
-      res.status(500).json({ message: err });
-    });
+    // extra -- check if id exists
+    const pid = req.params.id;
+    Product.deleteOne({ _id: id })
+        .then(function (result) {
+            res.status().json({ message: 'Place deleted' });
+        })
+        .catch(function (error) {
+            res.status(500).json({ message: err });
+        });
 });
 
 //for update
 
 router.put('/place/update', function (req, res) {
-  const placename = req.body.placename;
-  const placeprice = req.body.placeprice;
-  const placedesc = req.body.placedesc;
+    const placename = req.body.placename;
+    const placeprice = req.body.placeprice;
+    const placedesc = req.body.placedesc;
 
-  const pid = req.body.id;
-  Place.updateOne(
-    { _id: pid },
-    {
-      placename: placename,
-      placeprice: placeprice,
-      placedesc: placedesc,
-     
-    }
-  )
-    .then(function (result) {
-      console.log(result);
-      res.status(200).json({ message: 'Place Updated Successfully!!' });
-    })
-    .catch(function (e) {
-      console.log(e);
-      res.status(500).json({ message: e });
+    const pid = req.body.id;
+    Place.updateOne(
+        { _id: pid },
+        {
+            placename: placename,
+            placeprice: placeprice,
+            placedesc: placedesc,
+
+        }
+    )
+        .then(function (result) {
+            console.log(result);
+            res.status(200).json({ message: 'Place Updated Successfully!!' });
+        })
+        .catch(function (e) {
+            console.log(e);
+            res.status(500).json({ message: e });
+        });
+});
+router.get('/place/fetch', function (req, res) {
+    Place.find().then(function (placeData) {
+        res
+            .status(200)
+            .json({ success: true, count: placeData.length, data: placeData });
     });
 });
 
-
-
+//for single page view
+router.get('/place/single/:id', function (req, res) {
+    const id = req.params.id;
+    Place.findOne({ _id: id })
+        .then(function (data) {
+            console.log(data);
+            res.status(200).json(data);
+        })
+        .catch(function (e) {
+            res.status(500).json({ error: e });
+        });
+})
 module.exports = router;
