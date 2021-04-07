d7830dab1d185475c6051c5c4decc072dfb373fd
Sanjeet <sanjeetpandey010@gmail.com>
2021-04-07 13:27:22 +0545

    minor changed on middlaware

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const trip_db = require('./database/trip_db');
 const customersRoute = require('./routes/customersRoute');
 const bookingRoute = require('./routes/bookingRoute');
 const contactRoute = require('./routes/contactRoute');
+const placeRoute = require('./routes/placeRoute');
+const path = require('path');
 // const publicDir = path.join(_dirname,'public');
 // app.use(express.static(publicDir))
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'image')));
 app.use(customersRoute);
 app.use(bookingRoute);
 app.use(contactRoute);
+app.use(placeRoute);
 app.listen(90);
 
