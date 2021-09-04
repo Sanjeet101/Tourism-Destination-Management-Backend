cf6f756c2c9b133e251d10d46f274af888df1352
Sanjeet101 <57610989+Sanjeet101@users.noreply.github.com>
2021-09-04 20:45:41 +0545

    backend done

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,24 +1,24 @@
-// const mongoose = require('mongoose');
-// const Contact = mongoose.model('Contact', {
-//     firstname: {
-//         type: String
-//     },
-//     lastname: {
-//         type: String,
+const mongoose = require('mongoose');
+const Contact = mongoose.model('Contact', {
+    firstname: {
+        type: String
+    },
+    lastname: {
+        type: String,
        
-//     },
-//     email: {
-//         type: String,
-//         unique : true
-//     },
-//     subject: {
-//         type: String,
+    },
+    email: {
+        type: String,
+        unique : true
+    },
+    subject: {
+        type: String,
         
-//     },
-//     message: {
-//         type: String,
+    },
+    message: {
+        type: String,
       
-//     }
+    }
     
-// })
-// module.exports = Contact;
+})
+module.exports = Contact;
