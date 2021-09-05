74dbb44f0503f8906e924865da27c8a61b640bec
Sanjeet101 <57610989+Sanjeet101@users.noreply.github.com>
2021-09-05 20:21:12 +0545

    images added

--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -1,26 +1,24 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination : function(req,file,cb){
-        cb(null, './image/images')
-    },
-    filename : function(req,file,cb){
-        cb(null, Date.now() + file.originalname)
-    }
-})
-
-const fileFilter = function(req,file,cb){
-    if(file.mimetype == 'image/jpeg'){
-        cb(null,true)
-    }
-    else{
-        cb(null,false)
-    }
-}
-const uploadImage = multer({
-    storage : storage,
-    fileFilter : fileFilter
+  destination: function (req, file, cb) {
+    cb(null, "./image/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
 });
 
+const fileFilter = function (req, file, cb) {
+  if (file.mimetype == "image/jpeg") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const uploadImage = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
 
 module.exports = uploadImage;
