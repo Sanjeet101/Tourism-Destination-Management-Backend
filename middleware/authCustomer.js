4af34ff3c5628dbb48bb4acd953a805aba09f0c7
Sanjeet101 <57610989+Sanjeet101@users.noreply.github.com>
2021-09-05 19:36:20 +0545

    customers model and route added

--- middleware/authCustomer.js
+++ middleware/authCustomer.js
@@ -1,34 +1,34 @@
-const jwt = require('jsonwebtoken');
-const Customers = require('../models/customersModel');
-//main guard
-module.exports.verifyCustomer = function(req,res, next){
-    try{
-        const token = req.headers.authorization.split(" ")[1];
-        const data = jwt.verify(token, 'secretkey');
-        Customers.findOne({_id : data.CustomerId})
-        .then(function(result){
-            //success
-            req.customer = result;
-            next();
-        })
-        .catch(function(result){
-            //invalid
-            res.status('403').json({errror : " Customer Auth Failed"})
-        })
+// const jwt = require('jsonwebtoken');
+// const Customers = require('../models/customersModel');
+// //main guard
+// module.exports.verifyCustomer = function(req,res, next){
+//     try{
+//         const token = req.headers.authorization.split(" ")[1];
+//         const data = jwt.verify(token, 'secretkey');
+//         Customers.findOne({_id : data.CustomerId})
+//         .then(function(result){
+//             //success
+//             req.customer = result;
+//             next();
+//         })
+//         .catch(function(result){
+//             //invalid
+//             res.status('403').json({errror : " Customer Auth Failed"})
+//         })
        
-    }
-    catch(error){
-        res.status('403').json({errror : error})
-    }
-}
-//second guard
-module.exports.verifyAdmin = function(req,res,next){
-    if(!req.customer){
-        return res.status(401).json({message : "not allowed!! "})
-    }
-    else if(req.customer.accountType!=='Admin'){
-        return res.status(401).json({message : "permission denied!!"})
-    }
-    next();
+//     }
+//     catch(error){
+//         res.status('403').json({errror : error})
+//     }
+// }
+// //second guard
+// module.exports.verifyAdmin = function(req,res,next){
+//     if(!req.customer){
+//         return res.status(401).json({message : "not allowed!! "})
+//     }
+//     else if(req.customer.accountType!=='Admin'){
+//         return res.status(401).json({message : "permission denied!!"})
+//     }
+//     next();
 
-}
+// }
--- middleware/imageUpload.js
+++ middleware/imageUpload.js
@@ -1,26 +1,26 @@
-const multer = require('multer');
+// const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination : function(req,file,cb){
-        cb(null, './image/images')
-    },
-    filename : function(req,file,cb){
-        cb(null, Date.now() + file.originalname)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination : function(req,file,cb){
+//         cb(null, './image/images')
+//     },
+//     filename : function(req,file,cb){
+//         cb(null, Date.now() + file.originalname)
+//     }
+// })
 
-const fileFilter = function(req,file,cb){
-    if(file.mimetype == 'image/jpeg'){
-        cb(null,true)
-    }
-    else{
-        cb(null,false)
-    }
-}
-const uploadImage = multer({
-    storage : storage,
-    fileFilter : fileFilter
-});
+// const fileFilter = function(req,file,cb){
+//     if(file.mimetype == 'image/jpeg'){
+//         cb(null,true)
+//     }
+//     else{
+//         cb(null,false)
+//     }
+// }
+// const uploadImage = multer({
+//     storage : storage,
+//     fileFilter : fileFilter
+// });
 
 
-module.exports = uploadImage;
+// module.exports = uploadImage;
